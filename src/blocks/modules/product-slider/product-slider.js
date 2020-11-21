11dd8c19c1da6610a2ff8d872040a21cf70cee5f
Kenny <nobodykenny@gmail.com>
2020-11-21 04:29:25 +0300

    'work'

--- src/blocks/modules/product-slider/product-slider.js
+++ src/blocks/modules/product-slider/product-slider.js
@@ -1,2 +1,44 @@
+import Swiper from 'swiper';
 
+document.addEventListener('DOMContentLoaded', function() {
+	(function initSlider() {
+		let sliderTop = document.querySelector('.product-slider__top-slider');
+		let sliderThumb = document.querySelector('.product-slider__bottom-slider');
 
+		let length = sliderThumb.querySelectorAll('.product-slider__bottom-slide').length;
+		console.log(length);
+
+		if (length <= 1) {
+			// var galleryTop = new Swiper(sliderTop, {
+			// 	slideActiveClass: 'product-slider__top-slide--active',
+			// });
+
+			sliderThumb.classList.add('product-slider__bottom-slider--hidden');
+		} else {
+			let galleryThumbs = new Swiper(sliderThumb, {
+				spaceBetween: 16,
+				slidesPerView: 3,
+				watchSlidesVisibility: true,
+				watchSlidesProgress: true,
+				loop: true,
+				loopedSlides: 4,
+				slideToClickedSlide: true,
+				centeredSlides: true,
+				slideActiveClass: 'product-slider__bottom-slide--active'
+			});
+
+			let galleryTop = new Swiper(sliderTop, {
+				loop: true,
+				loopedSlides: 4,
+				slideActiveClass: 'product-slider__top-slide--active',
+				thumbs: {
+					swiper: galleryThumbs
+				}
+			});
+
+			galleryTop.controller.control = galleryThumbs;
+			galleryThumbs.controller.control = galleryTop;
+		}
+
+	})();
+})
--- src/js/import/modules.js
+++ src/js/import/modules.js
@@ -1,2 +1,3 @@
 import "%modules%/product-slider/product-slider";
 import "%modules%/item-contact/item-contact";
+import "%modules%/product-form/product-form";
