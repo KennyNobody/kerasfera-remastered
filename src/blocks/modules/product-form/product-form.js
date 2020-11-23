14bdeb702f819b803e466d4c6147ef03fea9dff6
Kenny <nobodykenny@gmail.com>
2020-11-23 08:56:39 +0300

    'work'

--- src/blocks/modules/product-form/product-form.js
+++ src/blocks/modules/product-form/product-form.js
@@ -1,13 +1,32 @@
-// modules.define('product-form', ['i-bem-dom'], function(provide, bemDom) {
+document.addEventListener('DOMContentLoaded', function() {
+	let form = document.querySelector('.product-form');
 
-// provide(bemDom.declBlock(this.name, {
-//     onSetMod: {
-//         js: {
-//             inited: function() {
-                
-//             }
-//         }
-//     }
-// }));
+	let btnPlus = form.querySelector('.product-form__counter-button--plus');
+	let btnMinus = form.querySelector('.product-form__counter-button--minus');
 
-// });
+	let area = form.querySelector('#product-type-s').value;
+	let count = form.querySelector('#product-type-count').value;
+	let packs = form.querySelector('#product-type-pack').value;
+
+	btnPlus.addEventListener('click', function(e) {
+		e.preventDefault();
+		changeState();
+	});
+
+	btnMinus.addEventListener('click', function(e) {
+		e.preventDefault();
+		changeState();
+	});
+
+	function changeState() {
+		console.log('Изменить стейт');
+	}
+
+	function showResult() {
+		console.log('Площадь: ' + area);
+		console.log('Количество штук: ' + count);
+		console.log('Количество упаковок: ' + packs);
+	}
+
+	showResult();
+});
